1e7451cf7c0f930220c905bd8e22975896420279
cutsea110 <cutsea110@gmail.com>
2012-06-26 08:01:45 +0900

    efficient matching of weighted regular expressions

--- RegExpPlay.hs
+++ RegExpPlay.hs
@@ -1,6 +1,7 @@
 module RegExpPlay where
 
 import Prelude hiding (sum)
+import Data.List (foldl')
 
 data Reg = Eps         -- ε
          | Sym Char    -- α
@@ -27,6 +28,7 @@ parts [c] = [[[c]]]
 parts (c:cs) =
   concat [[(c:p):ps,[c]:p:ps]|p:ps <- parts cs]
 
+-- ((a|b)*c(a|b)*c)*(a|b)*
 evencs :: Reg
 evencs = Seq (Rep (Seq onec onec)) nocs
   where
@@ -77,3 +79,86 @@ instance Semiring Int where
   one = 1
   (<+>) = (+)
   (<*>) = (*)
+
+
+data REG = EPS
+         | SYM Bool Char
+         | ALT REG REG
+         | SEQ REG REG
+         | REP REG
+
+shift :: Bool -> REG -> Char -> REG
+shift _ EPS _ = EPS
+shift m (SYM _ x) c = SYM (m && x == c) x
+shift m (ALT p q) c = ALT (shift m p c) (shift m q c)
+shift m (SEQ p q) c = SEQ (shift m p c) (shift (m && empty p || final p) q c)
+shift m (REP r) c = REP (shift (m || final r) r c)
+
+empty :: REG -> Bool
+empty EPS = True
+empty (SYM _ _) = False
+empty (ALT p q) = empty p || empty q
+empty (SEQ p q) = empty p && empty q
+empty (REP _) = True
+
+final :: REG -> Bool
+final EPS = False
+final (SYM b _) = b
+final (ALT p q) = final p || final q
+final (SEQ p q) = final p && empty q || final q
+final (REP r) = final r
+
+match :: REG -> String -> Bool
+match r [] = empty r
+match r (c:cs) = final (foldl' (shift False) (shift True r c) cs)
+
+data REGw c s = REGw { emptyw :: s
+                     , finalw :: s
+                     , regw   :: REw c s
+                     }
+data REw c s = EPSw
+             | SYMw (c -> s)
+             | ALTw (REGw c s) (REGw c s)
+             | SEQw (REGw c s) (REGw c s)
+             | REPw (REGw c s)
+
+epsw :: Semiring s => REGw c s
+epsw = REGw { emptyw = one
+            , finalw = zero
+            , regw   = EPSw
+            }
+
+symw :: Semiring s => (c -> s) -> REGw c s
+symw f = REGw { emptyw = zero
+              , finalw = zero
+              , regw   = SYMw f
+              }
+
+altw :: Semiring s => REGw c s -> REGw c s -> REGw c s
+altw p q = REGw { emptyw = emptyw p <+> emptyw q
+                , finalw = finalw p <+> finalw q
+                , regw = ALTw p q
+                }
+           
+seqw :: Semiring s => REGw c s -> REGw c s -> REGw c s
+seqw p q = REGw { emptyw = emptyw p <*> emptyw q
+                , finalw = finalw p <*> emptyw q <+> finalw q
+                , regw = SEQw p q
+                }
+
+repw :: Semiring s => REGw c s -> REGw c s
+repw r = REGw { emptyw = one
+              , finalw = finalw r
+              , regw = REPw r
+              }
+
+matchw :: Semiring s => REGw c s -> [c] -> s
+matchw r [] = emptyw r
+matchw r (c:cs) = finalw (foldl' (shiftw zero . regw) (shiftw one (regw r) c) cs)
+
+shiftw :: Semiring s => s -> REw c s -> c -> REGw c s
+shiftw _ EPSw       _ = epsw
+shiftw m (SYMw f)   c = (symw f) { finalw = m <*> f c }
+shiftw m (ALTw p q) c = altw (shiftw m (regw p) c) (shiftw m (regw q) c)
+shiftw m (SEQw p q) c = seqw (shiftw m (regw p) c) (shiftw (m <*> emptyw p <+> finalw p) (regw q) c)
+shiftw m (REPw r)   c = repw (shiftw (m <+> finalw r) (regw r) c)
