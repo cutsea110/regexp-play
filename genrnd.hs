349862f88ffef814dba30d8a937650d8d914c3e5
Katsutoshi Itoh <cusea110@gmail.com>
2012-06-28 12:49:14 +0900

    delete used key from cache

--- genrnd.hs
+++ genrnd.hs
@@ -18,12 +18,13 @@ genrnd' g w len = f streamR 0 Map.empty BL.empty
       | n == len  = BL.reverse ys
       | otherwise = 
         if Map.member n ns
-        then f rrs (n+1) ns $ 'b' `BL.cons` ys
-        else f rs (n+1) ns' $ r `BL.cons` ys
+        then f rrs (n+1) ns' $ 'b' `BL.cons` ys
+        else f rs (n+1) ns'' $ r `BL.cons` ys
             where
               r = BL.head rrs
               rs = BL.tail rrs
-              ns' = if r=='a' then Map.insert (n+w) undefined ns else ns
+              ns' = Map.delete n ns
+              ns'' = if r=='a' then Map.insert (n+w) undefined ns else ns
 
 genrnd :: RandomGen g => g -> Int -> Int -> String
 genrnd g w l = BL.unpack $ genrnd' g w ((w+1)*(l+1))
