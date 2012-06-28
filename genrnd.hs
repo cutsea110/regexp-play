292e17f2eea5eb0eae74cee9dc5d16a4310074cc
Katsutoshi Itoh <cusea110@gmail.com>
2012-06-28 10:09:08 +0900

    Refactor:Arrange cache by using Data.Map

--- genrnd.hs
+++ genrnd.hs
@@ -1,24 +1,24 @@
 module Main where
 
 import System.Random
-import Data.List
+import qualified Data.Map as Map
 
 randoms' :: RandomGen g => g -> [Char]
 randoms' g = (\(x,g') -> x:randoms' g') (randomR ('a','b') g)
 
 genrnd' :: RandomGen g => g -> Int -> Int -> [Char]
-genrnd' g w len = f streamR 0 [] []
+genrnd' g w len = f streamR 0 Map.empty []
   where
     streamR = randoms' g
-    f :: [Char] -> Int -> [Int] -> [Char] -> [Char]
+    f :: [Char] -> Int -> Map.Map Int a -> [Char] -> [Char]
     f rrs@(r:rs) n ns ys
       | n == len  = reverse ys
       | otherwise = 
-        case find (n==) ns of
-          Just _  -> f rrs (n+1) ns ('b':ys)
-          Nothing -> f rs (n+1) ns' (r:ys)
+        if Map.member n ns
+        then f rrs (n+1) ns ('b':ys)
+        else f rs (n+1) ns' (r:ys)
             where
-              ns' = if r=='a' then ((n+w):ns) else ns
+              ns' = if r=='a' then Map.insert (n+w) undefined ns else ns
 
 genrnd :: RandomGen g => g -> Int -> Int -> [Char]
 genrnd g n l = genrnd' g n ((n+1)*(l+1))
